d8a86a06ee1c5b3b9630077ab6b5058866749948
Pramod R <pramodsagu04@gmail.com>
2025-02-20 15:21:18 +0530

    Run Changes

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -6,8 +6,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginService {
 
-  //apiurl='https://localhost:7000/api/Auth/login';
-  apiurl='https://business-11.onrender.com/api/Auth/login';
+  apiurl='https://localhost:7000/api/Auth/login';
+  // apiurl='https://business-11.onrender.com/api/Auth/login';
 
   constructor(private http: HttpClient) { }
 
