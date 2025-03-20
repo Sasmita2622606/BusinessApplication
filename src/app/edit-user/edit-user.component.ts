f8e8326589049af9d5cf9887ac78d028ff152eef
Pramod R <pramodsagu04@gmail.com>
2025-03-20 12:17:24 +0530

    Edit - Bussiness - HTML

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { BusinessService } from '../service/business.service';
 
@@ -23,31 +23,44 @@ export class EditUserComponent {
   editCustomerForm!: FormGroup;
   categories: any;
   subCategories: any;
+  center: google.maps.LatLngLiteral = { lat: 0, lng: 0 }; // Default to San Francisco
+  zoom = 10;
+  marker: google.maps.LatLngLiteral | null = null;
+  fileName: string | undefined;
+  imagePreview: string | undefined;
+  fileUpload: any;
   constructor(private fb: FormBuilder, private businessService: BusinessService) {
-    
+   
   }
+
   ngOnInit() {
-    this.roleID = Number(localStorage.getItem('roleId')); // Get role from local storage
+    this.roleID = Number(localStorage.getItem('roleId'));
     this.domainID = Number(localStorage.getItem('domainID'));
-    this.getDetailsByID(this.roleID,this.domainID);
+  
     if (this.roleID === 3) {
-      this.getCategories();
-      // Business Form
+      // ✅ Initialize Form First
       this.editBusinessForm = this.fb.group({
-        businessName: ['', [Validators.required, Validators.minLength(3)]],
-        email: ['', [Validators.required, Validators.email]],
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        emailId: ['', [Validators.required, Validators.email]],
         description: ['', [Validators.required, Validators.maxLength(500)]],
-        location: ['', Validators.required]
+        categoryID: ['', [Validators.required]],
+        subCategoryID: ['', [Validators.required]]
       });
+  
+      // Then Fetch Data
+      this.getCategories();
+      this.getDetailsByID(this.roleID, this.domainID);
     } else if (this.roleID === 4) {
-      // Customer Form
       this.editCustomerForm = this.fb.group({
         name: ['', [Validators.required, Validators.minLength(3)]],
-        email: ['', [Validators.required, Validators.email]],
-        location: ['', Validators.required]
+        email: ['', [Validators.required, Validators.email]]
       });
+  
+      this.getDetailsByID(this.roleID, this.domainID);
     }
   }
+  
+  
 
   submitBusinessForm() {
     if (this.editBusinessForm.valid) {
@@ -61,34 +74,63 @@ export class EditUserComponent {
     }
   }
   
-  getDetailsByID(roleID: number,domainID: number)
-  {
-    if(roleID == 3){
-      this.businessService.getBusinessDetailById(domainID).subscribe((data)=>{
+  getDetailsByID(roleID: number, domainID: number) {
+    if (roleID === 3) {
+      this.businessService.getBusinessDetailById(domainID).subscribe((data) => {
         this.businessDetails = data;
-        console.log("BS Details: ", this.businessDetails)
-      })
-    }
-    else if(roleID == 4){
-      this.businessService.getCustomerDetailsByID(domainID).subscribe((data)=>{
+        console.log("Business Details: ", this.businessDetails);
+  
+        // Ensure data exists before patching
+        if (this.businessDetails && this.editBusinessForm) {
+          this.editBusinessForm.patchValue({
+            name: this.businessDetails.name  || '',
+            emailId: this.businessDetails.emailId || '',
+            description: this.businessDetails.description || '',
+            // location: this.businessDetails.location || '',
+            categoryID: this.businessDetails.categoryID || '',
+            subCategoryID: this.businessDetails.subCategoryID || ''
+          });
+        }
+      });
+    } else if (roleID === 4) {
+      this.businessService.getCustomerDetailsByID(domainID).subscribe((data) => {
         this.customerDetails = data;
-        console.log("CX Details: ", this.customerDetails)
-      })
+        console.log("Customer Details: ", this.customerDetails);
+  
+        if (this.customerDetails && this.editCustomerForm) {
+          this.editCustomerForm.patchValue({
+            name: this.customerDetails.name || '',
+            email: this.customerDetails.emailId || ''
+            // location: this.customerDetails.location || ''
+          });
+        }
+      });
     }
-
   }
-
+  
+  
   getCategories(): void {
     this.businessService.getCategories().subscribe((data) => {
       this.categories = data;
       console.log("Categories:", this.categories);
-      this.getSubCategories();
+      // this.getSubCategories();
     });
   }
 
-  getSubCategories() {
-    this.businessService.getSubCategories(this.editBusinessForm.value.CategoryID).subscribe((result: any) => {
-      this.subCategories = result;
-    });
+  // getSubCategories() {
+  //   this.businessService.getSubCategories(this.businessDetails.categoryID).subscribe((result: any) => {
+  //     this.subCategories = result;
+  //   });
+  // }
+
+  onCategoryChange(eve: any): void {
+    this.editBusinessForm.controls['categoryID'].setValue(eve.target.value)
+    this.editBusinessForm.controls['subCategoryID'].setValue('');
+    // this.getSubCategories();
   }
+
+   onSubCategoryChange(eve: any): void {
+     this.editBusinessForm.controls['subCategoryID'].setValue(eve.target.value)
+   }
+
 }
