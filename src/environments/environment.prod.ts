a66033ef06b8613d59ee497d2ef30b838f1e9dee
Sasmita Sahu <sasmitasahu19793@gmail.com>
2025-03-21 16:26:59 +0530

    API Key changes for Map

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_KEY: 'AlzaSymgj1DcUTL879lBFLjRhHMrLijEtrFCXYu',
+    API_KEY: 'AlzaSy-jduE11xr_YpIm8w412sVvf3oyl5hHSzb',
     baseUrl:'https://business-11.onrender.com/api/'
 
   };
