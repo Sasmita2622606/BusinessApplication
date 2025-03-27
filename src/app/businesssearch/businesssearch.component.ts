ab3f10e7dcbe6eef5b532fd6574d13abc073af80
guntiakshitha <guntiakshitha2@gmail.com>
2025-03-27 15:24:07 +0530

    Changes had made in header of buisness search and change password dropdown.

--- src/app/businesssearch/businesssearch.component.ts
+++ src/app/businesssearch/businesssearch.component.ts
@@ -79,6 +79,8 @@ export class BusinesssearchComponent implements OnInit {
   roleID: string | null = null;
   isModalOpen = false;
   modalImageUrl = '';
+  distanceAscending:boolean = false;
+  ratingAscending:boolean=false;
   constructor(private fb: FormBuilder, private businessService: BusinessService, private router: Router, private authservice: AuthService, private renderer: Renderer2) { }
 
   ngOnInit(): void {
@@ -102,7 +104,6 @@ export class BusinesssearchComponent implements OnInit {
     this.getCustomerDetails();
     this.roleID = this.authservice.getRoleIdFromToken();
     console.log("token", this.roleID)
-
   }
 
   openModal(imageUrl: string) {
@@ -392,7 +393,6 @@ export class BusinesssearchComponent implements OnInit {
    }else{
     this.businessList =this.businessList?.sort((a,b)=>b.distancekm-a.distancekm);
    }
-
   }
   sortRating(value:boolean):any{
     if(value && this.businessList.length>0){
@@ -520,5 +520,16 @@ export class BusinesssearchComponent implements OnInit {
         console.error('update error:', error);
       }
     });
+  }
+
+  toggleSortDistance() {
+    this.distanceAscending = !this.distanceAscending;
+    this.sortDistance(this.distanceAscending);
+  }
+
+  toggleSortRating()
+  {
+    this.ratingAscending = !this.ratingAscending;
+    this.sortRating(this.ratingAscending);
   }
 }
